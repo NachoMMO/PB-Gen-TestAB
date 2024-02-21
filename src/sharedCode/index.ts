0619d9d03e1b879e905c79b216bec0a23292d6df
Nacho Martínez <ignaciommo@ext.inditex.com>
2024-02-21 22:33:52 +0100

    se ha añadido el sharedCode

--- src/sharedCode/index.ts
+++ src/sharedCode/index.ts
@@ -6,34 +6,37 @@ const inditex = window['inditex'] || {};
 const Backbone = window['Backbone'] || {};
 
 const main = () => {
-  try {
-    const parsedMetrics = parseMetrics(metrics_keys);
+  const parsedMetrics = parseMetrics(metrics_keys);
+  const productsVisited: Array<string> = [];
 
-    const isCategoryInExperiment = () => {
-      return document.querySelector('html')?.classList.contains('search-active');
-    }
-
-    const optimizely = new Optimizely(parsedMetrics, experimentCode, isCategoryInExperiment);
-
-    if (inditex.iPage === 'ItxOrderConfirmationPage') {
-      optimizely.trackConfirmationRevenue();
-    }
-
-    optimizely.bindSearchProductClicked();
-    optimizely.bindSearchImpressionEvents();
-
-    Backbone.Radio.channel('search').on('open-search', () => {
-      optimizely.registerVisits()
-    });
-
-    Backbone.Radio.channel('productBus').on('product:addToCart', (data) => {
-      optimizely.trackAddToCartSearchFromParrilla(data)
-    });
-  } catch (err) {
-    console.error(err);
+  const isCategoryInExperiment = () => {
+    return inditex.iXProductInfo;
   }
+
+  const isProductVisited = (productId: string) => {
+    return productsVisited.includes(productId);
+  }
+
+  const optimizely = new Optimizely(parsedMetrics, experimentCode);
+
+  if (inditex.iPage === 'ItxOrderConfirmationPage') {
+    optimizely.trackConfirmationRevenue();
+  }
+
+  optimizely.bindPDPSizeSelectedClicked();
+
+  Backbone.Radio.channel('productBus').on('product:addToCart', (data) => {
+    optimizely.trackAddToCartFromPDP(data)
+  });
+
+  Backbone.Radio.channel('productBus').on('product:load', () => {
+    if (isCategoryInExperiment() && !isProductVisited(inditex.iXProductInfo?.id)) {
+      productsVisited.push(inditex.iXProductInfo?.id);
+      optimizely.registerVisits()
+    }
+  });
 };
 
-if (Backbone && inditex.isMobileDevice()) {
+if (Backbone) {
   main();
 }

d78c901cd08a417bdaa4844ae2c77667af5cd229
Nacho Martínez <ignaciommo@ext.inditex.com>
2024-02-23 15:07:27 +0100

    Se ha modificado el fichero index del SharedCode para hacerlo mas estándar. Se ha añadido la condición de terminar el proceso de creación del test para que no se quede pensando hasta el infinito

--- src/sharedCode/index.ts
+++ src/sharedCode/index.ts
@@ -6,34 +6,19 @@ const inditex = window['inditex'] || {};
 const Backbone = window['Backbone'] || {};
 
 const main = () => {
-  try {
-    const parsedMetrics = parseMetrics(metrics_keys);
+  const parsedMetrics = parseMetrics(metrics_keys);
 
-    const isCategoryInExperiment = () => {
-      return document.querySelector('html')?.classList.contains('search-active');
-    }
+  const isCategoryInExperiment = () => {
+    return true;
+  }
 
-    const optimizely = new Optimizely(parsedMetrics, experimentCode, isCategoryInExperiment);
+  const optimizely = new Optimizely(parsedMetrics, experimentCode, isCategoryInExperiment);
 
-    if (inditex.iPage === 'ItxOrderConfirmationPage') {
-      optimizely.trackConfirmationRevenue();
-    }
-
-    optimizely.bindSearchProductClicked();
-    optimizely.bindSearchImpressionEvents();
-
-    Backbone.Radio.channel('search').on('open-search', () => {
-      optimizely.registerVisits()
-    });
-
-    Backbone.Radio.channel('productBus').on('product:addToCart', (data) => {
-      optimizely.trackAddToCartSearchFromParrilla(data)
-    });
-  } catch (err) {
-    console.error(err);
+  if (inditex.iPage === 'ItxOrderConfirmationPage') {
+    optimizely.trackConfirmationRevenue();
   }
 };
 
-if (Backbone && inditex.isMobileDevice()) {
+if (Backbone) {
   main();
 }
